efdfa28d82fa5eb1a4e68293985a54694f1e3e4d
Paolo Chang <chanhwan.chang@gmail.com>
2021-03-06 00:21:41 -0500

    Add login

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import client from "../client"
 
 export default {
@@ -40,6 +41,35 @@ export default {
                 });
             } catch (e) {
                 return e;
+            }
+        },
+        login: async(_, { username, password }) => {
+
+            /** (1) find the user with args.username */
+            const user = await client.user.findFirst({
+                where: { username }
+            })
+
+            if (!user) {
+                return {
+                    status: false, error: "Username is not valid."
+                }
+            }
+
+            /** (2) check password with args.password */
+            const validation = await bcrypt.compare(password, user.password);
+
+            if (!validation) {
+                return {
+                    status: false, error: "Password is not valid."
+                }
+            }
+
+            /** (3) issue and return a token to the user */
+            const token = await jwt.sign({id:user.id}, process.env.PRIVATE_KEY);
+
+            return {
+                status: true, token: token
             }
         }
     }
