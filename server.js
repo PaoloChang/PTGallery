1423da7535479586f153aae351bc296670c7c65e
Paolo Chang <chanhwan.chang@gmail.com>
2021-03-06 04:26:41 -0500

    Add token authentication and protect resolver

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 import { ApolloServer } from 'apollo-server';
 import schema from './schema';
+import { getUser, protectedResolver } from './users/users.utils';
 
 const PORT = process.env.PORT;
 
 const server = new ApolloServer({
-    schema
+    schema,
+    context: async ({ req }) => {
+        return {
+            loggedInUser: await getUser(req.headers.token),
+            protectedResolver,
+        };
+    }
 });
 
 server
