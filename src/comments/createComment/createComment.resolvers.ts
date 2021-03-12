449a98fef94d9b77399fdf15bb73fb230591b9a6
Paolo Chang <chanhwan.chang@gmail.com>
2021-03-12 02:19:04 -0500

    Add createComment

---
+++ src/comments/createComment/createComment.resolvers.ts
@@ -0,0 +1,38 @@
+import { Resolvers } from "../../types";
+import { protectedResolver } from "../../users/users.utils";
+
+const resolvers: Resolvers = {
+    Mutation: {
+        createComment: protectedResolver( async (_, { photoId, payload }, { client, loggedInUser }) => {
+            const photo = await client.photo.findUnique({
+                where: { id: photoId },
+                select: { id: true }
+            });
+
+            if (!photo) {
+                return {
+                    status: false,
+                    error: "Photo is not found."
+                }
+            }
+
+            await client.comment.create({
+                data: {
+                    payload,
+                    user: {
+                        connect: { id: loggedInUser.id }
+                    },
+                    photo: {
+                        connect: { id: photoId}
+                    },
+                }
+            });
+
+            return {
+                status: true
+            };
+        })
+    }
+}
+
+export default resolvers;
