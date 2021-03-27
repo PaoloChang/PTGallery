ba07e1b752dc5a23f7be928a3ff470aad2ec88fe
Paolo Chang <chanhwan.chang@gmail.com>
2021-03-26 21:22:32 -0400

    Add display photo in descending order

--- src/users/users.resolvers.ts
+++ src/users/users.resolvers.ts
@@ -1,56 +1,61 @@
-import client from "../client"
+import client from "../client";
 
 export default {
-    User: {
-        /** In first argument root has current user information */
-        photos: ({ id }, { lastId }) => {
-            return client.user.findUnique({ 
-                where: { id }
-            }).photos({
-                take: 5,
-                skip: lastId ? 1 : 0,
-                ...(lastId && { cursor : { id: lastId }})
-            })
+  User: {
+    /** In first argument root has current user information */
+    photos: ({ id }, { lastId }) => {
+      return client.user
+        .findUnique({
+          where: { id },
+        })
+        .photos({
+          orderBy: {
+            createdAt: "desc",
+          },
+          take: 5,
+          skip: lastId ? 1 : 0,
+          ...(lastId && { cursor: { id: lastId } }),
+        });
+    },
+    totalFollowing: ({ id }) =>
+      client.user.count({
+        where: {
+          followers: {
+            some: { id },
+          },
         },
-        totalFollowing: ({ id }) => 
-            client.user.count({ 
-                where: { 
-                    followers: {
-                        some: { id }
-                    }
-                }
-            }),
-        totalFollowers: ({ id }) => 
-            client.user.count({
-                where: {
-                    following: {
-                        some: { id }
-                    }
-                }
-            }),
-        isMine: ({ id }, _, { loggedInUser }) => {
-            if (!loggedInUser) {
-                return false;
-            }
-            return id === loggedInUser?.id;
+      }),
+    totalFollowers: ({ id }) =>
+      client.user.count({
+        where: {
+          following: {
+            some: { id },
+          },
         },
-        isFollowing: async ({ id }, _, { loggedInUser }) => {
-            if (!loggedInUser) {
-                return false;
-            }
+      }),
+    isMine: ({ id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      return id === loggedInUser?.id;
+    },
+    isFollowing: async ({ id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
 
-            const exist = await client.user.count({
-                where: {
-                    username: loggedInUser.username,
-                    following: {
-                        some: {
-                            id
-                        }
-                    }
-                }
-            })
-            
-            return Boolean(exist);
-        }
-    }
-}
+      const exist = await client.user.count({
+        where: {
+          username: loggedInUser.username,
+          following: {
+            some: {
+              id,
+            },
+          },
+        },
+      });
+
+      return Boolean(exist);
+    },
+  },
+};
