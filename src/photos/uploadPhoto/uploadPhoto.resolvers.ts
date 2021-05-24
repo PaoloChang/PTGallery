6306e01b7f060b7c0d5db78be3bce77651678ece
Paolo Chang <chanhwan.chang@gmail.com>
2021-05-23 23:59:04 -0400

    Update uploadPhoto

--- src/photos/uploadPhoto/uploadPhoto.resolvers.ts
+++ src/photos/uploadPhoto/uploadPhoto.resolvers.ts
@@ -4,46 +4,51 @@ import { protectedResolver } from "../../users/users.utils";
 import { extractHashtagsFrom } from "../photos.utils";
 
 const resolvers: Resolvers = {
-    Mutation: {
-        uploadPhoto: protectedResolver( async (
-            _, 
-            { image, caption },
-            { client, loggedInUser }
-        ) => {
+  Mutation: {
+    uploadPhoto: protectedResolver(
+      async (_, { image, caption }, { client, loggedInUser }) => {
+        console.log(`uploadPhoto`);
+        console.log(image);
+        console.log(caption);
 
-            let hashtagObjs = [];
+        let hashtagObjs = [];
 
-            if (caption) {
-                hashtagObjs = extractHashtagsFrom(caption);
-            }
-            
-            const imageURL = await uploadImageToS3(image, loggedInUser.id, "galleries");
+        if (caption) {
+          hashtagObjs = extractHashtagsFrom(caption);
+        }
+        console.log(`hashtagObjs: ${hashtagObjs}`);
 
-            await client.photo.create({
-                data: {
-                    image: imageURL,
-                    caption,
-                    user: {
-                        connect: {
-                            id: loggedInUser.id
-                        }
-                    },
-                    ...(hashtagObjs.length > 0 && {
-                        hashtags: {
-                            connectOrCreate: hashtagObjs
-                        }
-                    })
-                }
-            })
+        const imageURL = await uploadImageToS3(
+          image,
+          loggedInUser.id,
+          "galleries"
+        );
 
-            // save the photo with the parsed hashtags
-            // add the photo to the hashtags
-            return {
-                status: true,
-                image
-            }
-        })
-    }
-}
+        const photo = await client.photo.create({
+          data: {
+            image: imageURL,
+            caption,
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
+            },
+            ...(hashtagObjs.length > 0 && {
+              hashtags: {
+                connectOrCreate: hashtagObjs,
+              },
+            }),
+          },
+        });
+
+        console.log(`photo created`);
+
+        // save the photo with the parsed hashtags
+        // add the photo to the hashtags
+        return photo;
+      }
+    ),
+  },
+};
 
 export default resolvers;
--- src/photos/uploadPhoto/uploadPhoto.typeDefs.ts
+++ src/photos/uploadPhoto/uploadPhoto.typeDefs.ts
@@ -1,12 +1,12 @@
 import gql from "graphql-tag";
 
 export default gql`
-    type UploadPhotoResult {
-        status: Boolean!
-        error: String
-        image: Photo
-    }
-    type Mutation {
-        uploadPhoto(image:Upload!, caption:String): UploadPhotoResult
-    }
+  type UploadPhotoResult {
+    status: Boolean!
+    error: String
+    image: Photo
+  }
+  type Mutation {
+    uploadPhoto(image: Upload!, caption: String): Photo
+  }
 `;
