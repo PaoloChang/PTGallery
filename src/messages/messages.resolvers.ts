d89be505b9eb03f9bbd7ec313ebc206d213e4713
Paolo Chang <chanhwan.chang@gmail.com>
2021-06-01 22:35:11 -0400

    Add isMine on Message

--- src/messages/messages.resolvers.ts
+++ src/messages/messages.resolvers.ts
@@ -1,35 +1,40 @@
 import client from "../client";
 import { Resolvers } from "../types";
 
-const resolvers: Resolvers = {
-    Room: {
-        users: ({ id }) => client.room.findUnique({ where: { id }}).users(),
-        messages: ({ id }, { lastId }) => client.message.findMany({ 
-            where: { roomId: id },
-            take: 10,
-            skip: lastId ? 1 : 0,
-            ...(lastId && { cursor : { id: lastId }})
-        }),
-        unreadTotal: ({ id }, _, { loggedInUser }) => {
-            if (!loggedInUser) {
-                return 0;
-            }
-            return client.message.count({
-                where: {
-                    roomId: id,
-                    user: {
-                        id: {
-                            not: loggedInUser.id
-                        }
-                    },
-                    read: false
-                }
-            });
-        }
+export default {
+  Room: {
+    users: ({ id }) => client.room.findUnique({ where: { id } }).users(),
+    messages: ({ id }, { lastId }) =>
+      client.message.findMany({
+        where: { roomId: id },
+        take: 20,
+        skip: lastId ? 1 : 0,
+        ...(lastId && { cursor: { id: lastId } }),
+      }),
+    unreadTotal: ({ id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return 0;
+      }
+      return client.message.count({
+        where: {
+          roomId: id,
+          user: {
+            id: {
+              not: loggedInUser.id,
+            },
+          },
+          read: false,
+        },
+      });
     },
-    Message: {
-        user: ({ id }) => client.message.findUnique({ where: { id }}).user(),
-    }
-}
-
-export default resolvers;
+  },
+  Message: {
+    user: ({ id }) => client.message.findUnique({ where: { id } }).user(),
+    isMine: ({ userId }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      return userId === loggedInUser.id;
+    },
+  },
+};
