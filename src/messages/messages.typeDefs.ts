be9e2919d8502b61120b41b518f19f4eec9890c7
Paolo Chang <chanhwan.chang@gmail.com>
2021-06-01 22:23:28 -0400

    Add isMine on Message

--- src/messages/messages.typeDefs.ts
+++ src/messages/messages.typeDefs.ts
@@ -1,21 +1,22 @@
 import gql from "graphql-tag";
 
 export default gql`
-    type Room {
-        id: Int!
-        unreadTotal: Int!
-        users: [User]
-        messages: [Message]
-        createdAt: String!
-        updatedAt: String!
-    }
-    type Message {
-        id: Int!
-        payload: String!
-        user: User!
-        room: Room!
-        read: Boolean!
-        createdAt: String!
-        updatedAt: String!
-    }
+  type Room {
+    id: Int!
+    unreadTotal: Int!
+    users: [User]
+    messages: [Message]
+    createdAt: String!
+    updatedAt: String!
+  }
+  type Message {
+    id: Int!
+    payload: String!
+    user: User!
+    room: Room!
+    read: Boolean!
+    isMine: Boolean!
+    createdAt: String!
+    updatedAt: String!
+  }
 `;
